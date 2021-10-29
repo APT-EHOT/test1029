b5a428119e00d2422787bb8484cacfaada1bbf33
a.matchin <a.matchin@prequelapp.com>
2021-10-29 11:40:31 +0300

    Added logic to MainActivity

--- app/src/main/java/com/example/test1029/MainActivity.java
+++ app/src/main/java/com/example/test1029/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.test1029;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    EditText editText;
+
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btnMain);
+        textView = findViewById(R.id.tvMain);
+        editText = findViewById(R.id.etMain);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
     }
 }
